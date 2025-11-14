7ca81432c3de28faf3fd6e792d77fe4c8d5b94d7
dev_nick <developernikhil28@gmail.com>
2025-11-14 15:56:27 +0530

    Healthcheck route setup

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 
 const app = express();
-const port = 3000 || process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(express.json({limit:"16kb"}))
+
+// Healthcheck route
+
+app.get("/" , (req,res)=> {
+    res.send({
+        status:"ok",
+        service:"Payment Instruction Service"
+    })
+})
 
 export default app;
